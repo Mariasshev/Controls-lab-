d4fa36e44a63df431e11c6d184c1b5334b5923d5
Maria Shevchenko <sevcenkomaria72@gmail.com>
2024-02-13 21:03:05 +0200

    Добавьте файлы проекта.

---
+++ CheckBoxAndRadio/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// ?????????? ????, ????????? ? Microsoft Visual C++.
+// ???????????? CheckBoxAndRadio.rc
+//
+#define IDCANCEL2                       3
+#define IDD_DIALOG1                     101
+#define IDC_CHECK1                      1001
+#define IDC_CHECK2                      1002
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_RADIO3                      1005
+#define IDC_RADIO4                      1006
+#define IDC_RADIO5                      1007
+#define IDC_RADIO6                      1008
+#define IDC_EDIT1                       1009
+#define IDC_EDIT2                       1010
+#define IDC_EDIT3                       1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
